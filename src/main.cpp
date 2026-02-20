ac8642dd28ef1b995d8dfb0e0321e695d24f1abd
ranjanjs09 <purendra.ranjan_cs.aiml24@gla.ac.in>
2026-02-20 10:41:32 +0530

    Commit all code

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,39 @@
-#include <Arduino.h>
 /**
- * @file main.ino
+* @file main.cpp
  * @brief Embedded Temperature and Humidity Monitoring using DHT11
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Purendra Ranjan [ranjanjs09]
+ * @date 19/02/2026
  *
  * @details
  * This program reads environmental data from the DHT11 sensor
  * and displays temperature and humidity values on Serial Monitor.
  * Students must complete the TODO sections.
- */
-
+ **/
+#include <Arduino.h>
 #include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
 
-// TODO 1:
-// Define the DHT data pin (Use digital pin 2)
-
-// TODO 2:
-// Define the DHT sensor type (DHT11)
-
-// TODO 3:
-// Create a DHT object using the defined pin and sensor type
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-
-    // TODO 4:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 5:
-    // Initialize the DHT sensor
-
-    // TODO 6:
-    // Print a system initialization message
+    Serial.begin(9600);
+    dht.begin();
+    Serial.println("DHT11 Sensor Initialized");
 }
 
 void loop() {
-
-    // TODO 7:
-    // Read humidity value from sensor
-
-    // TODO 8:
-    // Read temperature value from sensor
-
-    // TODO 9:
-    // Check if either reading failed using isnan()
-    // If failed, print error message and return
-
-    // TODO 10:
-    // Print formatted temperature and humidity values
-
-    // TODO 11:
-    // Add a 2-second delay before next reading
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+    if (isnan(humidity) || isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        return;
+    }
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print("%\t");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println("°C");
+    delay(2000);
 }
